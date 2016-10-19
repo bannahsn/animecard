2d1e8a740af2b86c53d0cefd59681a072c6ae2fc
amrishparmar <amrish.parmar@me.com>
2016-10-19 18:40:34 +0000

    added form retrieval test code

--- animecard.js
+++ animecard.js
@@ -1,29 +1,10 @@
-(function() {
+$(document).ready(function(){
+    
     cardForm = {
         name: "",
         top5: [1, 2, 3, 4, 5],
     };
-    $.ajaxSetup({
-        error: function(jqXHR, exception) {
-            if (jqXHR.status === 0) {
-                alert('Not connect.\n Verify Network.');
-            } else if (jqXHR.status == 404) {
-                alert('Requested page not found. [404]');
-            } else if (jqXHR.status == 500) {
-                alert('Internal Server Error [500].');
-            } else if (exception === 'parsererror') {
-                alert('Requested JSON parse failed.');
-            } else if (exception === 'timeout') {
-                alert('Time out error.');
-            } else if (exception === 'abort') {
-                alert('Ajax request aborted.');
-            } else {
-                alert('Uncaught Error.\n' + jqXHR.responseText);
-            }
-        }
-    });
     var url = "https://hummingbird.me/api/v1/anime/2";
-    var query = "/anime/2";
     $.ajax({
         crossOrigin:true,
         url: url,
@@ -31,4 +12,13 @@
           console.log(data);
      }
     });
-}());
+    
+    $('#anime-form').submit(function(e) {
+            e.preventDefault();
+            var formData = $('#anime-form').serializeArray();
+            for (var i = 0; i < formDta.length; ++i) {
+                console.log(formData[i]['value']);
+            }
+    });
+
+});
