9da452447c2b435cb304fb7e58d0f20bd55400bf
Hasan Banna <hbann002@gold.ac.uk>
2016-10-19 19:02:44 +0100

    added a plugin for CORS

--- animecard.js
+++ animecard.js
@@ -3,15 +3,32 @@
         name: "",
         top5: [1, 2, 3, 4, 5],
     };
+    $.ajaxSetup({
+        error: function(jqXHR, exception) {
+            if (jqXHR.status === 0) {
+                alert('Not connect.\n Verify Network.');
+            } else if (jqXHR.status == 404) {
+                alert('Requested page not found. [404]');
+            } else if (jqXHR.status == 500) {
+                alert('Internal Server Error [500].');
+            } else if (exception === 'parsererror') {
+                alert('Requested JSON parse failed.');
+            } else if (exception === 'timeout') {
+                alert('Time out error.');
+            } else if (exception === 'abort') {
+                alert('Ajax request aborted.');
+            } else {
+                alert('Uncaught Error.\n' + jqXHR.responseText);
+            }
+        }
+    });
     var url = "https://hummingbird.me/api/v1/anime/2";
     var query = "/anime/2";
-    var aRequest = $.ajax({
+    $.ajax({
+        crossOrigin:true,
         url: url,
-        jsonp: "callback",
-        dataType: "json",
+        success: function(data) {
+          console.log(data);
+     }
     });
-    aRequest.done(function(response){
-      alert(response);
-    });
-
 }());
